6127e1d9eda27f967411aa918ec8d5902f8d9b35
leha-code <pezleha@gmail.com>
2021-10-24 19:22:50 -0400

    Add render function

--- main.py
+++ main.py
@@ -1,2 +1,9 @@
-#from libmineshaft import *
+#from libmineshaft import * # Currently won't be used
 import pygame
+
+class Engine:
+    def __init__(self,  world, blocks):
+        self.world = world
+        self.blocks = blocks
+    def render(self):
+        pass
