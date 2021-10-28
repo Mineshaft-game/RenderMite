8933b27b892a708887e67743e01974d2b3bf71f1
MayuSakurai <mayu2kura1@gmail.com>
2021-10-28 07:42:27 -0400

    Aaand...Working rendering engine!

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
-from libmineshaft import *
+import libmineshaft
 import pygame
 
 
 class Engine:
-    def __init__(self,  world, blocks):
-        self.world = world
-        self.blocks = blocks
+    def __init__(self, blockindex):
+        self.blockindex = blockindex
 
-    def render(self):
-        pass
+    def render(self,screen,world,pos=(0,0)):
+        for chunk in range(0,16):
+            for subchunk in range(0,225):
+                for block in range(0,16)::
+                    block = blockindex[world[chunk][subchunk][block]]
+                    image = pygame.image.load(block.image)
+                    screen.blit(image, ((chunk+block)*16,subchunk*16
