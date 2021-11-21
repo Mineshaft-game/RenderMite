3721cfe1570ebee4470dd0ec45bdf42329247e7b
leha-code <pezleha@gmail.com>
2021-11-21 18:10:02 -0500

    Fix the image problem

--- __init__.py
+++ __init__.py
@@ -4,8 +4,8 @@ import pygame
 
 class Engine:
     def __init__(self, blockindex):
-        __version__ = "unknown"
-        print(f"RenderMite {__version__}")
+        self.__version__ = "unknown"
+        print(f"RenderMite v[{self.__version__}]")
         self.blockindex = blockindex
 
     def render(self, screen, world: libmineshaft.world.World, pos=(0, 0)):
@@ -13,8 +13,8 @@ class Engine:
             for subchunk in range(0, 128):
                 for block in range(0, 16):
                     block = self.blockindex[world.world[chunk][subchunk][block]]
-                    if type(block.image) == list:
-                        image = pygame.transform.scale(pygame.image.load(block.image))
+                    if type(block.imagepath) == list:
+                        image = pygame.transform.scale(pygame.image.load(block.imagepath))
                         screen.blit(image, ((chunk + block) * 16, subchunk * 16))
                     elif block.image == False:
                         continue
