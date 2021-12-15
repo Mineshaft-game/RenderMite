7c0ec3d0e4c96ced0f616a54cdf0dc235946c3c6
leha-code <pezleha@gmail.com>
2021-12-14 19:57:29 -0500

    Reformat with black

--- __init__.py
+++ __init__.py
@@ -7,32 +7,28 @@ def joinpath(path: list):
     returnstr = str()
     for entry in path:
         returnstr = returnstr + os.path.sep + entry
-        
+
     return os.fspath(returnstr)
 
-#TODO: Make the rendering engine manage the graphics implementation
+
+# TODO: Make the rendering engine manage the graphics implementation
 class Engine:
     def __init__(self, blockindex):
         self.__version__ = "unknown"
-        #if os.environ["SHOW_RENDER_VERSION"] == "1": #check if the enviroment variable "SHOW_RENDER_VERSION" is set to 1
-        print(f"RenderMite v[{self.__version__}]") #print the splash
-        self.blockindex = blockindex 
+        # if os.environ["SHOW_RENDER_VERSION"] == "1": #check if the enviroment variable "SHOW_RENDER_VERSION" is set to 1
+        print(f"RenderMite v[{self.__version__}]")  # print the splash
+        self.blockindex = blockindex
 
     def render(self, screen, world: libmineshaft.world.World, pos=(0, 0)):
-        for chunk in range(0, 16): # For every chunk
-            for subchunk in range(0, 128): #For every subchunk
-                for block in range(0, 16): # for every block
+        for chunk in range(0, 16):  # For every chunk
+            for subchunk in range(0, 128):  # For every subchunk
+                for block in range(0, 16):  # for every block
                     block = self.blockindex[world.world[chunk][subchunk][block]]
                     if type(block.image) is list:
-                        
+
                         path = joinpath(block.image)
-                        print (path)
+                        print(path)
                         image = pygame.transform.scale(pygame.image.load())
                         screen.blit(image, ((chunk + block) * 16, subchunk * 16))
                     elif block.image is False:
                         continue
-                        
-                        
-
-
-
--- wrapper/music.py
+++ wrapper/music.py
@@ -3,6 +3,7 @@ import pygame.mixer
 
 import os
 
+
 def init_music():
     if pygame.mixer.get_init() is None:
         pygame.mixer.init()
@@ -10,29 +11,35 @@ def init_music():
 
 def load_music(pathobj: os.PathLike):
     pygame.mixer.music.load(pathobj)
-    
+
+
 def play_music():
     pygame.mixer.music.play()
-    
+
+
 def stop_music():
     pygame.mixer.music.stop()
-    
+
+
 def get_busy():
     return pygame.mixer.music.get_busy()
-    
-    
+
+
 def unpause_music():
     pygame.mixer.music.unpause()
-    
+
+
 def set_volume(vol):
     pygame.mixer.music.set_volume(vol)
-    
+
+
 def get_volume():
     return pygame.mixer.music.get_volume()
 
+
 def unload_music():
     pygame.mixer.music.unload()
-    
+
+
 def queue_music(pathobj: os.PathLike):
     pygame.mixer.music.queue(pathobj)
-    
