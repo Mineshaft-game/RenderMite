5edca310461ada1b1192e065e055b56ec191a8b5
leha-code <pezleha@gmail.com>
2021-12-31 20:15:18 -0500

    Fix rendering engine lmao

--- __init__.py
+++ __init__.py
@@ -1,23 +1,29 @@
+
+__version__ = "unknown"
+
+
 import libmineshaft.world
 import pygame
 import os
 
 
-def joinpath(path: list):
-    returnstr = str()
+def joinimagepath(path: list,  assets_dir: str): 
+    returnstr = assets_dir
     for entry in path:
         returnstr = returnstr + os.path.sep + entry
+        print(returnstr)
 
-    return os.fspath(returnstr)
+    return returnstr
 
 
 # TODO: Make the rendering engine manage the graphics implementation
 class Engine:
-    def __init__(self, blockindex):
-        self.__version__ = "unknown"
+    def __init__(self, blockindex,  assets_dir):
+        self.__version__ = __version__
         # if os.environ["SHOW_RENDER_VERSION"] == "1": #check if the enviroment variable "SHOW_RENDER_VERSION" is set to 1
         print(f"RenderMite v[{self.__version__}]")  # print the splash
         self.blockindex = blockindex
+        self.assets_dir = assets_dir
 
     def render(self, screen, world: libmineshaft.world.World, pos=(0, 0)):
         for chunk in range(0, 16):  # For every chunk
@@ -26,9 +32,9 @@ class Engine:
                     block = self.blockindex[world.world[chunk][subchunk][block]]
                     if type(block.image) is list:
 
-                        path = joinpath(block.image)
+                        path = joinimagepath(block.image,  self.assets_dir)
                         print(path)
-                        image = pygame.transform.scale(pygame.image.load())
-                        screen.blit(image, ((chunk + block) * 16, subchunk * 16))
+                        image = pygame.transform.scale(pygame.image.load(path),  (16, 16))
+                        screen.blit(image, (16 * 16, subchunk * 16))
                     elif block.image is False:
                         continue
