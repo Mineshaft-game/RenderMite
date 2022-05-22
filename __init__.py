a564c1a05674fb5cc8f1c65bab2ee39a93eb714b
leha-code <pezleha@duck.com>
2022-05-22 13:23:11 -0400

    Fix table name

--- __init__.py
+++ __init__.py
@@ -11,7 +11,7 @@ import os
 class Engine:
     def __init__(self, blockindex, assets_dir):
         self.__version__ = __version__
-        # if os.environ["SHOW_RENDER_VERSION"] == "1": #check if the enviroment variable "SHOW_RENDER_VERSION" is set to 1
+        #if os.environ["SHOW_RENDER_VERSION"] == "1": #check if the enviroment variable "SHOW_RENDER_VERSION" is set to 1
         print(f"RenderMite v[{self.__version__}]")  # print the splash
         self.blockindex = blockindex
         self.assets_dir = assets_dir
@@ -19,7 +19,7 @@ class Engine:
     def render(self, screen, world: libmineshaft.world.World, pos=(0, 0)):
         for x in range(0, 256):
             for y in range(0, 128): 
-                block_type,  block_data,  x, y=world.database.execute("SELECT * FROM blocks WHERE x=? AND y=?", (x,y))
+                block_type,  block_data,  x, y=world.database.execute("SELECT * FROM world WHERE x=? AND y=?", (x,y))
                 block = self.blockindex[block_type]()
                 image =  pygame.subsurface(pygame.image.load(os.path.join(self.assets_dir, "terrain.png")), block.imagecoords, (16,16))
                 screen.blit(image, (pos+x*16,  pos+y*16))
